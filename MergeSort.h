6d7c587b21c76adc3d68bffa6ce7c956736744a7
root <root@DESKTOP-76RAP40.localdomain>
2021-02-04 23:32:37 -0700

    coded main, mergesort .h &.c output needs tweaking

---
+++ MergeSort.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void MergeSort(float [],int LeftIndex, int RightIndex);
+void Merge(float Array[], int LeftIndex, int Middle,int RightIndex);
+
+void print(float Array[],int ArraySize);
